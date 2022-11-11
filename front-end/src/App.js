de87495b9f6aa01565426e66b67402add06fc576
tuftsm <student.mtolo@gmail.com>
2022-11-11 01:29:24 +0000

    stuck on name

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 function App() {
   // setup state
@@ -71,6 +73,14 @@ function App() {
       setError("error decreasing" + error);
     }
   }  
+  
+  const getName = async(item) => {
+    try {
+      await axios.get("/api/cart/" + item.id);
+    } catch (error) {
+      setError("error decreasing" + error);
+    }
+  }
 
   // fetch ticket data
   useEffect(() => {
@@ -104,27 +114,40 @@ function App() {
     await decreaseCart(item);
     fetchCart();
   }  
+  
+  const idToName = async(item) => {
+    const data = getName(item);
+    return data.name;
+  }
+
+
 
   // render results
   return (
     <div className="App">
       {error}
-      <h1>Cart</h1>
-      {cart.map( item => (
-        <div key={item.id}>
-            {item.id}, {item.quantity}
-            <button onClick={e => decrease(item)}> - </button>
-            <button onClick={e => increase(item)}> + </button>
-            <button onClick={e => removeFromCart(item)}>Remove From Cart</button>          
-        </div>
-      ))}         
-      <h1>Products</h1>
-      {products.map( product => (
-        <div key={product.id}>
-            {product.name}, {product.price}
-          <button onClick={e => addToCart(product)}>Add To Cart</button>
-        </div>
-      ))}     
+      <Row>
+        <Col>
+          <h1>Products</h1>
+          {products.map( product => (
+            <div key={product.id}>
+              {product.name}, {product.price}
+              <button onClick={e => addToCart(product)}>Add To Cart</button>
+            </div>
+          ))}   
+        </Col>
+        <Col>
+          <h1>Cart</h1>
+          {cart.map( item => (
+            <div key={item.id}>
+                {idToName(item)}, {item.id}, {item.quantity}
+                <button onClick={e => decrease(item)}> - </button>
+                <button onClick={e => increase(item)}> + </button>
+                <button onClick={e => removeFromCart(item)}>Remove From Cart</button>          
+            </div>
+          ))}     
+        </Col>      
+      </Row>
     </div>
   );
 }
